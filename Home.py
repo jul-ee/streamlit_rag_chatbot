8ac4cba736d0c28ff5b11269ff02601cbd5e4fc8
jul-ee <160218278+jul-ee@users.noreply.github.com>
2025-06-26 20:48:29 +0900

    fix: 진입 시 pysqlite3 치환 테스트

--- Home.py
+++ Home.py
@@ -1,9 +1,9 @@
-# import sys
-# try:
-#     import pysqlite3
-#     sys.modules["sqlite3"] = pysqlite3
-# except ImportError:
-#     pass
+import sys
+try:
+    import pysqlite3
+    sys.modules["sqlite3"] = pysqlite3
+except ImportError:
+    pass
 
 import streamlit as st
 
